5e63b75c041d069a7992b971dd99ead729e5cc50
Theodore DeRego <tderego94@gmail.com>
2014-08-13 14:01:23 -1000

    should work with python 2.6

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import sys
 from src.fasta_reader import read_fasta
 
 def main():
-    with open(sys.argv[1], "r") as locations, open(sys.argv[2], "r") as fasta:
-        locations = [int(line) for line in locations]
-        seqs = read_fasta(fasta)
-        print("Location\t"+"\t".join([seq.header for seq in seqs]))
-        for location in locations:
-            print(str(location)+"\t"+"\t".join([seq.bases[location-1] for seq in seqs]))
+    with open(sys.argv[1], "r") as locations:
+        with open(sys.argv[2], "r") as fasta:
+            locations = [int(line) for line in locations]
+            seqs = read_fasta(fasta)
+            print("Location\t"+"\t".join([seq.header for seq in seqs]))
+            for location in locations:
+                print(str(location)+"\t"+"\t".join([seq.bases[location-1] for seq in seqs]))
 
 ##################################3
 
